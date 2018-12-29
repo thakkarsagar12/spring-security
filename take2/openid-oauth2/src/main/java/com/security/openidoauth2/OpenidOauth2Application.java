c7942e3cc84a22aa8c864d5db041c41ba53fc046
thakkarsagar12 <thakkarsagar12@gmail.com>
2018-12-29 20:04:45 +0530

    saf

---
+++ take2/openid-oauth2/src/main/java/com/security/openidoauth2/OpenidOauth2Application.java
@@ -0,0 +1,14 @@
+package com.security.openidoauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenidOauth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenidOauth2Application.class, args);
+    }
+
+}
+
